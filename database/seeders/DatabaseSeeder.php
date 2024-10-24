d2e6f15ef7ea9641249fb6dd6d5166aebc72bea8
victor <victor.rodrigues@multidadosti.com.br>
2024-10-24 20:44:21 -0300

    add proposals and validations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Models\User;
@@ -18,8 +19,13 @@ class DatabaseSeeder extends Seeder
          User::factory()->count(200)->create();
 
          User::query()->inRandomOrder()->limit(10)->get()->each(function(User $u){
+            
             $project = Project::factory()->create(['created_by' => $u->id]);
+            
             Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+            ArrangePositions::run($project->id);
+            
          });
 
          
